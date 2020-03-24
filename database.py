972cb1109d71816fd77b61642f8316a995f04651
Alexander Dransfield <alex@dransfield.me>
2020-03-24 16:00:56 +0000

    Adding initial files; Dockerifles, basic reminders add and list from csv

---
+++ database.py
@@ -0,0 +1,22 @@
+import csv
+
+def list_reminders():
+    f = open("reminders.csv", "r")
+
+    with f:
+        reader = csv.reader(f)
+
+        for row in reader:
+            print()
+            for e in row:
+                print(e.ljust(32), end=' ')
+        print()
+
+def add_reminder(reminder):
+    print()
+    reminder = input("What would you like to be reminded about?: ")
+    due = input("When is your reminder due?: ")
+    
+    with open('reminders.csv', 'a+', newline='\n') as file:
+        writer = csv.writer(file)
+        writer.writerow([reminder, due])
