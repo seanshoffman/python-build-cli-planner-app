2141de499bfc6d7b7d7b02d6de82e9cf5e08cc60
ReTeam Labs <reteamlabs@gmail.com>
2020-04-03 16:24:47 +0100

    Adding test tag, renaming test

--- tests/tests.py
+++ tests/tests.py
@@ -1,8 +1,9 @@
 import pytest
 from src.regular_reminder import RegularReminder
 
+@pytest.mark.task_one_regular_class
 class TestApp():
-    def test_task_one(self):
+    def test_task_one_regular_class(self):
         with pytest.raises(NotImplementedError) as e:
             regular = RegularReminder()
             assert regular.__str__(), "Failed to raise the correct error type"
