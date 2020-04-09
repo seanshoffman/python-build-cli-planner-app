9aaf728d335db5a1a152180c3ccddbbe3b13526f
ReTeam Labs <reteamlabs@gmail.com>
2020-04-09 23:29:38 +0100

    Updating to __iter__

--- tests/tests.py
+++ tests/tests.py
@@ -7,8 +7,8 @@ class TestApp():
     def test_task_one_regular_class_1(self):
         with pytest.raises(NotImplementedError) as e:
             regular = RegularReminder()
-            assert regular.__str__(), "Failed to raise the correct error type"
-        assert str(e.value) == "Method not implemented", "Error message does not match 'Method not implemented' for method __str__"
+            assert regular.__iter__(), "Failed to raise the correct error type"
+        assert str(e.value) == "Method not implemented", "Error message does not match 'Method not implemented' for method __iter__"
 
     @pytest.mark.task_one_regular_class_2
     def test_task_one_regular_class_2(self):
@@ -27,4 +27,4 @@ class TestApp():
     def test_task_two_abc_meta_class_2(self):
         with pytest.raises(TypeError) as e:
             abc_meta = ABCMetaReminder()
-            assert str(e.value) == "Can't instantiate abstract class ABCMetaReminder with abstract methods __str__, is_due", "The Abstract Base Class has not been correctly implemented with abstractmethods __str__, is_due"
+            assert str(e.value) == "Can't instantiate abstract class ABCMetaReminder with abstract methods __iter__, is_due", "The Abstract Base Class has not been correctly implemented with abstractmethods __iter__, is_due"
