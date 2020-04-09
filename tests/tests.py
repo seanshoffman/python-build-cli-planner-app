99aef7d47ba1e1dc1ade979c048f5b81a8353c61
ReTeam Labs <reteamlabs@gmail.com>
2020-04-09 19:00:06 +0100

    Removing __iter__

--- tests/tests.py
+++ tests/tests.py
@@ -8,10 +8,3 @@ class TestApp():
             regular = RegularReminder()
             assert regular.__str__(), "Failed to raise the correct error type"
         assert str(e.value) == "Method not implemented", "Error message does not match 'Method not implemented' for method __str__"
-        
-    @pytest.mark.task_one_regular_class_2
-    def test_task_one_regular_class_2(self):
-        with pytest.raises(NotImplementedError) as e:
-            regular = RegularReminder()
-            assert regular.__iter__(), "Failed to raise the correct error type"
-        assert str(e.value) == "Method not implemented", "Error message does not match 'Method not implemented' for method __iter__"
