b3028c52496711bb4af2ef87a01328d88e221da6
ReTeam Labs <reteamlabs@gmail.com>
2020-04-27 20:32:57 +0100

    Updating error messages

--- tests/tests.py
+++ tests/tests.py
@@ -10,7 +10,7 @@ class TestApp():
         with pytest.raises(NotImplementedError) as e:
             regular = RegularReminder()
             assert regular.__iter__(), "Failed to raise the correct error type"
-        assert str(e.value) == "Method not implemented", "Error message does not match 'Method not implemented' for method __iter__"
+        assert str(e.value) == "Abstract method '__iter__' should not be called", "Error message does not match 'Method not implemented' for method __iter__"
 
     @pytest.mark.task_one_regular_class_2
     def test_task_one_regular_class_2(self):
