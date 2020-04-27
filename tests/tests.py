8492afbca9d0ef983d6e0cf01e68fec2eaf16c3f
ReTeam Labs <reteamlabs@gmail.com>
2020-04-27 21:33:40 +0100

    Updating based on patch

--- tests/tests.py
+++ tests/tests.py
@@ -1,17 +1,27 @@
 import pytest
-from regular_reminder import RegularReminder
+import inspect
 
-class TestApp():
-    @pytest.mark.task_one_regular_class_1
-    def test_task_one_regular_class_1(self):
-        with pytest.raises(NotImplementedError) as e:
-            regular = RegularReminder()
-            assert regular.__iter__(), "Failed to raise the correct error type"
-        assert str(e.value) == "Abstract method '__iter__' should not be called", "Error message does not match \"Abstract method '__iter__' should not be called\" for method __iter__"
+@pytest.mark.task_one_regular_class_1
+def test_task_one_exists():
+    import regular_reminder
+    assert 'RegularReminder' in dir(regular_reminder), 'Could not find class `RegularReminder` in regular_reminder.py'
+    assert inspect.isclass(regular_reminder.RegularReminder), '`RegularReminder` is not a class'
 
-    @pytest.mark.task_one_regular_class_2
-    def test_task_one_regular_class_2(self):
-        with pytest.raises(NotImplementedError) as e:
-            regular = RegularReminder()
-            assert regular.is_due(), "Failed to raise the correct error type"
-        assert str(e.value) == "Abstract method 'is_due' should not be called", "Error message does not match \"Abstract method 'is_due' should not be called\" for method is_due"
+@pytest.mark.task_one_regular_class_2
+@pytest.mark.parametrize('abstract_method_name', [
+    "__iter__",
+    "is_due"
+])
+def test_task_one_abstract_method(abstract_method_name):
+    from regular_reminder import RegularReminder
+    assert hasattr(RegularReminder, abstract_method_name), f'Could not find `{abstract_method_name}` in `RegularReminder`'
+
+    reminder = RegularReminder()
+    abstract_method = getattr(reminder, abstract_method_name)
+    assert inspect.ismethod(abstract_method), 'f`{abstract_method_name}` is not a method. Have you forgotten `self`?'
+
+    with pytest.raises(NotImplementedError) as e:
+        assert abstract_method(), "Failed to raise the correct error type"
+
+    assert str(e.value) == "Abstract method has no implementation", \
+           f"Error message does not match 'Abstract method has no implementation' for method `{abstract_method_name}`"
