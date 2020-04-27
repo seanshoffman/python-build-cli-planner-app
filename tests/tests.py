f476360cfd5c2e5ac86084ddf077ed5385f7b3a5
ReTeam Labs <reteamlabs@gmail.com>
2020-04-27 20:49:07 +0100

    Updating error messgaes in tests for task-one

--- tests/tests.py
+++ tests/tests.py
@@ -13,14 +13,14 @@ class TestApp():
         with pytest.raises(NotImplementedError) as e:
             regular = RegularReminder()
             assert regular.__iter__(), "Failed to raise the correct error type"
-        assert str(e.value) == "Abstract method '__iter__' should not be called", "Error message does not match 'Method not implemented' for method __iter__"
+        assert str(e.value) == "Abstract method '__iter__' should not be called", "Error message does not match \"Abstract method '__iter__' should not be called\" for method __iter__"
 
     @pytest.mark.task_one_regular_class_2
     def test_task_one_regular_class_2(self):
         with pytest.raises(NotImplementedError) as e:
             regular = RegularReminder()
             assert regular.is_due(), "Failed to raise the correct error type"
-        assert str(e.value) == "Abstract method 'is_due' should not be called", "Error message does not match 'Method not implemented' for method is_due"
+        assert str(e.value) == "Abstract method 'is_due' should not be called", "Error message does not match \"Abstract method 'is_due' should not be called\" for method is_due"
 
 
     @pytest.mark.task_two_abc_meta_class_1
