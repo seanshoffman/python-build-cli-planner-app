bd5c8b4b2a2d0125143ff4a66167c69d1cec35ca
ReTeam Labs <reteamlabs@gmail.com>
2020-04-29 11:56:34 +0100

    Parametrized task two tests

--- tests/tests.py
+++ tests/tests.py
@@ -39,19 +39,35 @@ def test_task_two_abc_meta_class_1():
     assert issubclass(tuple, ABCMetaReminder)
     assert isinstance((), ABCMetaReminder)
 
+def get_decorators(source):
+    decorators = {}
+    def visit_FunctionDef(node):
+        decorators[node.name] = []
+        for n in node.decorator_list:
+            name = ''
+            if isinstance(n, ast.Call):
+                name = n.func.attr if isinstance(n.func, ast.Attribute) else n.func.id
+            else:
+                name = n.attr if isinstance(n, ast.Attribute) else n.id
+
+            args = [a.s for a in n.args] if hasattr(n, 'args') else []
+            decorators[node.name].append((name, args))
+
+    node_iter = ast.NodeVisitor()
+    node_iter.visit_FunctionDef = visit_FunctionDef
+    node_iter.visit(ast.parse(inspect.getsource(source)))
+    return decorators
+
 @pytest.mark.task_two_abc_meta_class_3
 @pytest.mark.parametrize('abstract_method_name', [
     "__iter__",
     "is_due"
 ])
+# @pytest.mark.xfail(raises=TypeError, reason='because')
 def test_task_two_abc_meta_class_2(abstract_method_name):
     from abc_meta_reminder import ABCMetaReminder
     assert hasattr(ABCMetaReminder, abstract_method_name), f'Could not find `{abstract_method_name}` in `RegularReminder`'
-
     with pytest.raises(TypeError) as e:
         abc_meta = ABCMetaReminder()
-        abstract_method = getattr(reminder, abstract_method_name)
-        assert inspect.ismethod(abstract_method), 'f`{abstract_method_name}` is not a method. Have you forgotten `self`?'
-        assert abstract_method(), "Failed to raise the correct error type"
-        
+
     assert str(e.value) == "Can't instantiate abstract class ABCMetaReminder with abstract methods __iter__, is_due", "The Abstract Base Class has not been correctly implemented with abstractmethods __iter__, is_due"
