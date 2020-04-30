81fc0f96b3d465351fada36be328aa33efe865d1
ReTeam Labs <nicolae.caprarescu@gmail.com>
2020-04-30 02:01:59 +0200

    removed files of later tasks; will put them back later

--- src/text_reminder.py
+++
@@ -1,8 +0,0 @@
-from reminder import Reminder
-
-class TextReminder(Reminder):
-    def __init__(self, reminder):
-        self.reminder = "Hey, don't forget to " + reminder
-
-    def __iter__(self):
-        return self.reminder
